5b85846b1823992d5c28384fe79c854c0bf693fb
stefan.ginev <stefan.ginev@blockchain-solutions.co>
2019-09-13 17:20:27 +0000

    bigger smarter better -- multiple endpoints + custom methods

--- serve.js
+++ serve.js
@@ -6,17 +6,22 @@ const PORT = parseInt(process.argv[2])
 
 const timeout = ms => new Promise(res => setTimeout(res, ms))
 
-let current_mode_index = 0, current_mode = config.modes[0]
+let selected_endpoint_index = 2
 
 console.clear()
 
-async function respond(req, res) {
-  let mode = current_mode
-  
+for ( let endpoint of config.endpoints )
+  endpoint.current_mode_index = 0
+
+for ( let endpoint of config.endpoints )
+  for ( let method of endpoint.methods )
+    app[method]( endpoint.route, (req,res)=> respond(req,res,endpoint) )
+
+async function respond( req, res, cfg ) {
+  let mode = cfg.modes[cfg.current_mode_index]
   if ( mode.delay )
     for ( let i = 0 ; i < mode.delay ; i++ )
       await timeout(1000)
-
   res.status( mode.status ).send( mode.text )
 }
 
@@ -26,36 +31,46 @@ stdin.resume();
 stdin.setEncoding('utf8');
 
 stdin.on('data', function(key){
-  if (key == '\u001B\u005B\u0044') // left
-    current_mode_index = (current_mode_index-1+config.modes.length)%config.modes.length
-  if (key == '\u001B\u005B\u0043') // right
-    current_mode_index = (current_mode_index+1)%config.modes.length
-  if (key == '\u001B\u005B\u0041') // up
-    process.stdout.write('up')
-  if (key == '\u001B\u005B\u0042') // down
-    process.stdout.write('down')
+  let endpoint = config.endpoints[selected_endpoint_index]
 
-  current_mode = config.modes[current_mode_index]
+  if (key == '\u001B\u005B\u0044') // left
+    endpoint.current_mode_index = cycleSelection( endpoint.modes, endpoint.current_mode_index, -1 )
+  if (key == '\u001B\u005B\u0043') // right
+    endpoint.current_mode_index = cycleSelection( endpoint.modes, endpoint.current_mode_index, 1 )
+  if (key == '\u001B\u005B\u0041') // up
+    selected_endpoint_index = cycleSelection( config.endpoints, selected_endpoint_index, -1 )
+  if (key == '\u001B\u005B\u0042') // down
+    selected_endpoint_index = cycleSelection( config.endpoints, selected_endpoint_index, 1 )
+
   draw()
-  
+
   if (key == '\u0003') { process.exit(); }    // ctrl-c
 })
-
-app.get( '/*', respond )
-app.post( '/*', respond )
+function cycleSelection( array, current, delta )
+{ return [ current + delta + array.length ] % array.length }
 
 function draw() {
-  let topbar = "", c, name
-  for ( let i = 0 ; i < config.modes.length ; i++ )
+  let endpoint = config.endpoints[selected_endpoint_index]
+
+  let eplist = "", ep
+  for ( let i = 0 ; i < config.endpoints.length ; i++ )
   {
-    c = config.modes[i]
+    ep = config.endpoints[i]
+    eplist += `\x1b[${i==selected_endpoint_index?'96;1':'96;2'}mroute: ${ep.route}\x1b[0m\n`
+  }
+
+  let topbar = "", c, name
+  for ( let i = 0 ; i < endpoint.modes.length ; i++ )
+  {
+    c = endpoint.modes[i]
     name = c.name || c.status || "-?-"
-    topbar += `\x1b[${i==current_mode_index?'44;93;1':'94'}m ${name} \x1b[0m`
+    topbar += `\x1b[${i==endpoint.current_mode_index?'44;93;1':'94'}m ${name} \x1b[0m`
   }
 
   console.clear()
   console.log("\x1b[0;2mTest me on port \x1b[1m%s\x1b[0m\n", server.address().port)
+  console.log( eplist, '\n' )
   console.log( topbar, '\n' )
-  console.log( current_mode )
+  console.log( endpoint.modes[endpoint.current_mode_index] )
 }
 server = app.listen( PORT, () => draw() )
